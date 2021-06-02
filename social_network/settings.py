53097c7a8ad88d5878db613517c88969475b3066
Prasad Talamala <vtalamala@apple.com>
2021-06-02 23:48:14 +0530

    fixed issues of DB

--- social_network/settings.py
+++ social_network/settings.py
@@ -75,11 +75,8 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'social_network',
-        'USER': 'prasad',
-        'PASSWORD': 'prasad',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
